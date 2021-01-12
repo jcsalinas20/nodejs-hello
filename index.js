be2dca05ff3a52931b07daee34eaa81f6fbd6bb2
jcsalinas20 <jcsalinasn20@gmail.com>
2021-01-12 19:49:31 +0100

    cambio de forma al mostrar el resultado

--- index.js
+++ index.js
@@ -1,8 +1,6 @@
 const express = require("express")
 const bodyParser = require("body-parser")
 const app = express()
-let http = require('http');
-let fs = require('fs');
 
 app.use(bodyParser.json())
 app.use(
@@ -23,27 +21,14 @@ app.use(function (req, res, next) {
 })
 
 app.get("/", (req, res) => {
-    res.writeHead(200, {
-        'Content-Type': 'text/html'
-    });
-    fs.readFile('./index.html', null, function (error, data) {
-        if (error) {
-            res.writeHead(404);
-            res.write('Whoops! File not found!');
-        } else {
-            res.write(data);
-        }
-        res.end();
-    });
+    res.send('<form method="post">\
+    <input type="text" name="name">\
+    <input type="submit" value="Enviar">\
+    </form>');
 })
 
-app.post("/hola", (req, res) => {
-    res.writeHead(200, {
-        'Content-Type': 'application/json',
-        'X-Powered-By': 'bacon'
-    });
-    res.write("Hola " + req.body.name);
-    res.end();
+app.post("/", (req, res) => {
+    res.send("Hola " + req.body.name);
 })
 
 app.listen("1337", async () => {
